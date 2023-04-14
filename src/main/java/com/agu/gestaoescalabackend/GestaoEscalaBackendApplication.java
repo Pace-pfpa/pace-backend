cd1ea5cec316170199a153b12face854a0b711e6
afonsobarata <72926475+afonsobarata@users.noreply.github.com>
2023-04-14 15:39:21 -0300

    feat: test

--- src/main/java/com/agu/gestaoescalabackend/GestaoEscalaBackendApplication.java
+++ src/main/java/com/agu/gestaoescalabackend/GestaoEscalaBackendApplication.java
@@ -8,6 +8,7 @@ public class GestaoEscalaBackendApplication {
 
 	public static void main(String... args) {
 		SpringApplication.run(GestaoEscalaBackendApplication.class, args);
+		System.out.printLn("teste 123");
 	}
 
 }
